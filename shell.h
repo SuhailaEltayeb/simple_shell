ec97b9c044b4411833a9ca7b785c12f7e9d6eed6
SuhailaEltayeb <suhailaeltayeb@gmail.com>
2023-11-14 10:13:11 -0800

    update shell.h

--- shell.h
+++ shell.h
@@ -22,7 +22,7 @@ extern char **environ;
 ssize_t My_Getline(char **_lineptr, size_t *size, int str);
 char *My_Strtok(char *_String, const char *_Delemiter);
 size_t _Strcspn(const char *_Str1, const char *_Str2);
-char *_Strchr(const char *_Str, int _Char);
+const char *_Strchr(const char *_Str, int _Char);
 char *_Strdup(char *Str);
 int _Strcmp(char *Str1, char *Str2);
 
